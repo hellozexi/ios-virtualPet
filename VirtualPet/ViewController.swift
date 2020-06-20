bece663b0dcf8f423247a416440883f5673d22a4
mikeliu <liuzexi@wustl.edu>
2020-06-19 22:38:27 -0500

    Initial Commit

---
+++ VirtualPet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualPet
+//
+//  Created by Mike Liu on 6/19/20.
+//  Copyright © 2020 Mike Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
